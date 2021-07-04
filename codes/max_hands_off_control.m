6ec99f99aebbc0c2b90babf9f10b077afd164dd2
Masaaki Nagahara <nagahara@ieee.org>
2021-07-04 13:58:37 +0900

    Add files via upload

---
+++ codes/max_hands_off_control.m
@@ -0,0 +1,66 @@
+%% Initialization
+clear
+
+%% System model
+% Plant matrices
+A = [0,1,0;0,0,1;0,0,0];
+b = [0;0;1];
+d = length(b); %system size
+% initial states
+x0 = ones(d,1);
+% Horizon length
+T = 10;
+
+%% Time discretization
+% Discretization size
+n = 1000; % grid size
+h = T/n; % discretization interval
+% System discretization
+[Ad,bd] = c2d(A,b,h);
+% Matrix Phi
+Phi = zeros(d,n);
+v = bd;
+Phi(:,end) = v;
+for j = 1:n-1
+    v = Ad*v;
+    Phi(:,end-j) = v;
+end
+% Vector zeta
+zeta = -Ad^n*x0;
+
+%% Convex optimizaiton via ADMM
+mu = 2*n+d;
+Psi = [eye(n);eye(n);Phi];
+%M = (Psi'*Psi)\Psi';
+M = (0.5*eye(n) - 0.5*Phi'*inv(2*eye(d)+Phi*Phi')*Phi)*Psi';
+sat = @(x) sign(x).*min(abs(x),1);
+
+EPS = 1e-5; % if the residue < EPS then the iteration will stop
+MAX_ITER = 100000; % maximum number of iterations
+z = [zeros(2*n,1);zeta]; v = zeros(mu,1);
+r = zeta;
+k = 0;
+gamma = 0.05;
+
+tic
+while (norm(r)>EPS) & (k < MAX_ITER)
+    u = M*(z-v);
+    z0 = soft_thresholding(gamma,u+v(1:n));
+    z1 = sat(u+v(n+1:2*n));
+    z2 = zeta;
+    z = [z0;z1;z2];
+    v = v + Psi*u - z;
+    r = Phi*u - zeta;
+    k = k + 1;
+end
+cpt=toc;
+
+%% Plot
+figure;
+plot(0:T/n:T-T/n,u,'r','LineWidth',3);
+title('Sparse control');
+grid on;
+xlabel('t')
+ylabel('u(t)')
+
+
