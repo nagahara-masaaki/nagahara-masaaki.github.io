f6bfc3c78cc28023ed4bc677222bd8f14bad03ec
Masaaki Nagahara <nagahara@ieee.org>
2022-02-22 21:48:29 +0900

    Add files via upload

--- IEICEdemo.m
+++ IEICEdemo.m
@@ -67,7 +67,7 @@ xs_polished = X;
 tt = t(1):0.01:t(end);
 yy = polyval(x,tt);
 yyr = polyval(xr,tt);
-yys = polyval(xs,tt);    
+yys = polyval(xs,tt);
 
 figure;
 plot(tt,yy,'LineWidth',1);
@@ -101,3 +101,33 @@ xs
 
 disp('Polished solution by IHT')
 xs_polished
+
+function sv = soft_thresholding(lambda,v)
+
+[m,n]=size(v);
+mn = m*n;
+sv = zeros(m,n);
+for i = 1:mn
+    if abs(v(i))<=lambda
+        sv(i) = 0;
+    else
+        sv(i) = v(i) - sign(v(i))*lambda;
+    end
+end
+
+end
+
+function hv = hard_thresholding(lambda,v)
+
+[m,n]=size(v);
+mn = m*n;
+hv = zeros(m,n);
+for i = 1:mn
+    if abs(v(i))<=lambda
+        hv(i) = 0;
+    else
+        hv(i) = v(i);
+    end
+end
+
+end
